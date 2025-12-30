aed5357ffdcf67af94729eda281e4c310343a410
Apnimaa <patelkinjal1212@gmail.com>
2025-12-30 15:56:52 +0530

    Update serverV1.py

--- serverV1.py
+++ serverV1.py
@@ -1,10 +1,13 @@
 import getserver
 
 def connect_server():
-    server = getserver()
-    server.connect_v1()
+    try:
+        getserver.connect_v1()
+    except Exception as e:
+        print("ERROR:", e)
 
 if __name__ == "__main__":
     connect_server()
+
 
 
