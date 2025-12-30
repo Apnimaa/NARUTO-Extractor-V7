79a71eceb822e57f7e8b52ac76750dc47047a869
Apnimaa <patelkinjal1212@gmail.com>
2025-12-31 04:59:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from config import Config
 from pyrogram import Client, idle
-import asyncio
-import logging
+import asyncio, logging
 
-# Logging
 logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 LOGGER.info("Live log streaming to telegram.")
+plugins = dict(root="plugins")
+if __name__ == "__main__":
 
 # Bot client (GLOBAL)
 bot = Client(
@@ -21,16 +21,10 @@ bot = Client(
 
 # Main async function
 async def main():
-    await bot.start()
-    bot_info = await bot.get_me()
-    LOGGER.info(f"<--- @{bot_info.username} Started --->")
-    await idle()
-    await bot.stop()
-
-# Entry point
-if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    finally:
-        LOGGER.info("<--- Bot Stopped --->")
+        await bot.start()
+        bot_info = await bot.get_me()
+        LOGGER.info(f"<--- @{bot_info.username} Started --->")
+        await idle()
+    asyncio.get_event_loop().run_until_complete(main())
+    LOGGER.info("<--- Bot Stopped --->")
     
